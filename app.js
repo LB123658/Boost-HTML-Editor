c66411f088329433c1cdc34e3f0880723512f354
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-03-12 10:04:33 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+function hjgcghjkhgfg() { 
+var x =  
+            document.getElementById("editor").value; 
+            
+            document.getElementById( 
+              "z").innerHTML = x; 
+        } 
+
+function kajygbxkjyw() { 
+     document.getElementById("editor").style.visibility="hidden";
+     document.getElementById("z").style.width="100%"; 
+     document.getElementById("z").style.left="0px"; 
+}
+function kjybxdykwuydsbxryuwkeuy() { 
+     document.getElementById("editor").style.visibility="visible";
+     document.getElementById("z").style.width="50%"; 
+     document.getElementById("z").style.left="52%"; 
+}
