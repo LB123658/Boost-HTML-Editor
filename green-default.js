917374690e351db2d7d55f633ae5fedaa064c96d
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-04-07 17:20:14 -0500

    Create green-default.js

---
+++ green-default.js
@@ -0,0 +1,112 @@
+function styleGreen() {
+var z = document.getElementsByClassName("green-default");  
+//green default button
+  z[0].style.color = "#188038";
+  z[0].style.cursor = "pointer";
+  z[0].style.border = "2px solid #188038";
+  z[0].style.borderRadius = "7px";
+  z[0].style.height = "50px";
+  z[0].style.width = "20%";
+  z[0].style.background = "transparent";
+  z[0].style.fontSize = "18px";
+  z[0].style.minWidth = "80px";
+
+//second button
+  z[1].style.color = "#188038";
+  z[1].style.cursor = "pointer";
+  z[1].style.border = "2px solid #188038";
+  z[1].style.borderRadius = "7px";
+  z[1].style.height = "50px";
+  z[1].style.width = "20%";
+  z[1].style.background = "transparent";
+  z[1].style.fontSize = "18px";
+  z[1].style.minWidth = "80px";
+
+  //third button
+  z[2].style.color = "#188038";
+  z[2].style.cursor = "pointer";
+  z[2].style.border = "2px solid #188038";
+  z[2].style.borderRadius = "7px";
+  z[2].style.height = "50px";
+  z[2].style.width = "20%";
+  z[2].style.background = "transparent";
+  z[2].style.fontSize = "18px";
+  z[2].style.minWidth = "80px";
+
+  //fourth button
+  z[3].style.color = "#188038";
+  z[3].style.cursor = "pointer";
+  z[3].style.border = "2px solid #188038";
+  z[3].style.borderRadius = "7px";
+  z[3].style.height = "50px";
+  z[3].style.width = "20%";
+  z[3].style.background = "transparent";
+  z[3].style.fontSize = "18px";  
+  z[3].style.minWidth = "80px";
+
+  //fifth button
+  z[4].style.color = "#188038";
+  z[4].style.cursor = "pointer";
+  z[4].style.border = "2px solid #188038";
+  z[4].style.borderRadius = "7px";
+  z[4].style.height = "50px";
+  z[4].style.width = "20%";
+  z[4].style.background = "transparent";
+  z[4].style.fontSize = "18px";  
+  z[4].style.minWidth = "80px";
+  
+  //sixth button
+  z[5].style.color = "#188038";
+  z[5].style.cursor = "pointer";
+  z[5].style.border = "2px solid #188038";
+  z[5].style.borderRadius = "7px";
+  z[5].style.height = "50px";
+  z[5].style.width = "20%";
+  z[5].style.background = "transparent";
+  z[5].style.fontSize = "18px";  
+  z[5].style.minWidth = "80px";
+
+  //seventh button
+  z[6].style.color = "#188038";
+  z[6].style.cursor = "pointer";
+  z[6].style.border = "2px solid #188038";
+  z[6].style.borderRadius = "7px";
+  z[6].style.height = "50px";
+  z[6].style.width = "20%";
+  z[6].style.background = "transparent";
+  z[6].style.fontSize = "18px";  
+  z[6].style.minWidth = "80px";
+  
+  //eighth button
+  z[7].style.color = "#188038";
+  z[7].style.cursor = "pointer";
+  z[7].style.border = "2px solid #188038";
+  z[7].style.borderRadius = "7px";
+  z[7].style.height = "50px";
+  z[7].style.width = "20%";
+  z[7].style.background = "transparent";
+  z[7].style.fontSize = "18px";  
+  z[7].style.minWidth = "80px";
+  
+  //ninth button
+  z[8].style.color = "#188038";
+  z[8].style.cursor = "pointer";
+  z[8].style.border = "2px solid #188038";
+  z[8].style.borderRadius = "7px";
+  z[8].style.height = "50px";
+  z[8].style.width = "20%";
+  z[8].style.background = "transparent";
+  z[8].style.fontSize = "18px";  
+  z[8].style.minWidth = "80px";
+  
+  //tenth button
+  z[9].style.color = "#188038";
+  z[9].style.cursor = "pointer";
+  z[9].style.border = "2px solid #188038";
+  z[9].style.borderRadius = "7px";
+  z[9].style.height = "50px";
+  z[9].style.width = "20%";
+  z[9].style.background = "transparent";
+  z[9].style.fontSize = "18px";  
+  z[9].style.minWidth = "80px";
+}
